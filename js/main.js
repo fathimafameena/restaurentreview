7595fcd8c111ce33383bf88d7deb1802cd0f4c27
fathimafameena <fathima.fameena@yahoo.com>
2018-10-04 21:53:14 -0500

    added the media queries and viewport tag ,css grid  ,css flex layout to make it responsive

--- js/main.js
+++ js/main.js
@@ -156,6 +156,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
+
   const li = document.createElement('li');
 
   const image = document.createElement('img');
@@ -178,9 +179,9 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
-  return li
+  return li;
 }
 
 /**
