e1b41ef1dab8f5b229cf686d59d7e2dd2bbf4f45
fathimafameena <fathima.fameena@yahoo.com>
2018-10-06 21:10:33 -0500

    added the service worker for the page and made a cache storage to store all information to available offline

---
+++ js/swregister.js
@@ -0,0 +1,10 @@
+console.log("hello");
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(function() {
+		console.log('Registration worked!');
+	})
+	.catch(function() {
+		console.log('Registration failed!');
+	});
+}
